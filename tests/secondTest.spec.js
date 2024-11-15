a7c180d0a4f73aca64954df2d216b1b7da09679c
Facundo Tolosa <faku.iron@gmail.com>
2024-11-15 17:59:00 -0300

    Some updates on the test folder

--- tests/secondTest.spec.js
+++ tests/secondTest.spec.js
@@ -1,40 +1,15 @@
 const { test, expect } = require('@playwright/test');
-const { login } = require('../utils/helpers');
+const { login, logout } = require('../utils/helpers');
 
 test.describe('Users Login to platform', () => {
-	test('Sales should be able to login', async ({ page }) => {
-	await login(page, 'facu+sales@frequencyads.com', 'password001');
-	// Assert the user is logged in
-	await expect(page.locator('//li[@class="dropdown"]/a/span[2]')).toBeVisible();
-	await page.screenshot({ path: 'screenshots/loginPageSales.jpg' });
-	});
-	
-	test('Adops should be able to login', async ({ page }) => {
-	await login(page, 'facu+adops@frequencyads.com', 'password001');
-	// Assert the user is logged in
-	await expect(page.locator('//li[@class="dropdown"]/a/span[2]')).toBeVisible();
-	await page.screenshot({ path: 'screenshots/loginPageAdops.jpg' });
-	});
-	
-	test('Show Producer should be able to login', async ({ page }) => {
-	await login(page, 'facu+showproducer@frequencyads.com', 'password001');
-	// Assert the user is logged in
-	await expect(page.locator('//li[@class="dropdown"]/a/span[2]')).toBeVisible();
-	await page.screenshot({ path: 'screenshots/loginPageShow.jpg' });
-	});
-	
-	test('Network Producer should be able to login', async ({ page }) => {
-	await login(page, 'facu+networkproducer@frequencyads.com', 'password001');
-	// Assert the user is logged in
-	await expect(page.locator('//li[@class="dropdown"]/a/span[2]')).toBeVisible();
-	await page.screenshot({ path: 'screenshots/loginPageNetwork.jpg' });
-	});
-	
-	test('Admin should be able to login', async ({ page }) => {
-	await login(page, 'facu+admin@frequencyads.com', 'password001');
-	// Assert the user is logged in
-	await expect(page.locator('//li[@class="dropdown"]/a/span[2]')).toBeVisible();
-	await page.screenshot({ path: 'screenshots/loginPageAdmin.jpg' });
-	});
+	const users = ['facu+admin@frequencyads.com', 'facu+sales@frequencyads.com', 'facu+adops@frequencyads.com', 'facu+showproducer@frequencyads.com', 'facu+networkproducer@frequencyads.com'];
+	for (const user of users){
+		test(`"${user}" should be able to login and logout`, async({ page }) => {
+			await login(page, user, 'password001');
+			//await page.screenshot({ path: `screenshots/loginPage${user}.jpg` });
+			//logout
+			await logout(page);
+		});
+	};
 	
 });
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,33 @@
-const login = async (page, username, password) => {
-  await page.goto('/login'); // Using baseURL set in playwright.config.js
-  await page.fill('#username', username);
-  await page.fill('#password', password);
-  await page.click('//span[contains(text(), "Log in")]');
+const assert = require('assert');
+const { expect } = require('@playwright/test');
 
-  // Wait for successful login (you can adjust based on your app)
-  await page.waitForSelector('(//*[@class="ReactVirtualized__Grid__innerScrollContainer"])[2]'); // Assuming there's a dashboard after login
-  //await page.screenshot({ path: 'screenshots/loginPage.jpg' });
+const login = async (page, username, password) => {
+  try{
+		await page.goto('/login'); // Using baseURL set in playwright.config.js
+		await page.fill('#username', username);
+		await page.fill('#password', password);
+		await page.click('//span[contains(text(), "Log in")]');
+		// Wait for successful login
+		await expect(page.locator('//li[@class="dropdown"]/a/span[2]')).toBeVisible();
+		//await page.waitForSelector('(//*[@class="ReactVirtualized__Grid__innerScrollContainer"])[2]');
+		await page.screenshot({ path: `screenshots/loginPage${username}.jpg` });
+  } catch (error) {
+		console.error('Problem login in:', error);
+  }
+  
 };
 
-module.exports = { login };
+const logout = async (page) => {
+	try {
+			await page.click('//li[@class="dropdown"]/a/span[2]');
+			await page.click('//a[@href="/logout"]');
+			await expect(page.locator('//h3[@class="heading"]')).toBeVisible();
+			//await page.waitForSelector('//h3[@class="heading"]');
+			const isLoggedOut = await page.isVisible('//a[@href="/login"]');
+			assert.strictEqual(isLoggedOut, true, "Session not logged out");
+		} catch (error) {
+			console.error('Problem login out:', error);
+	}
+};
+
+module.exports = { login, logout };
